09dc2ffb4f84d38d90b08664a8c7cb065bfed5e0
mgutiereza <mgutierrez6@hotmail.com>
2022-10-07 23:13:21 -0500

    Estructura inicial de objetos de aplicación
    Interface y Objeto Entity

---
+++ APPMetaDataObj/objDataAPP/iObjetoAplicacion.py
@@ -0,0 +1,26 @@
+import string
+
+
+class iObjetoAplicacion:
+
+    _nombreTabla: string
+    _claseObjeto: string
+
+    def __init__(self, nombreTabla, claseObjeto):
+        self._nombreTabla = nombreTabla
+        self._claseObjeto = claseObjeto
+
+    def generarArchivo(self):
+        pass
+
+    def _generarClase(self):
+        pass
+
+    def _generarLibreriasClase(self):
+        pass
+
+    def _generarCabeceraClase(self):
+        pass
+
+    def _generarCuerpoClase(self):
+        pass
---
+++ APPMetaDataObj/objDataAPP/objetoEntity.py
@@ -0,0 +1,23 @@
+from urllib import response
+from iObjetoAplicacion import iObjetoAplicacion
+
+class objetoEntity(iObjetoAplicacion):
+
+    def __init__(self,nombreTabla,claseObjeto):
+        self._nombreTabla = nombreTabla
+        self._claseObjeto = claseObjeto
+
+    def generarArchivo(self):
+        return
+
+    def _generarClase(self):
+        return
+
+    def _generarLibreriasClase(self):
+        return
+
+    def _generarCabeceraClase(self):
+        return
+
+    def _generarCuerpoClase(self):
+        return
